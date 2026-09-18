acd437e7ae43033bc2ecc4a62540b8a35ab02a86
ArmandoJr-tech <armandojossiasantonio@gmail.com>
2026-09-18 13:50:47 -0700

    segunda versao do aplicativo

--- minha_idade.py
+++ minha_idade.py
@@ -1,7 +1,8 @@
 idade=int(input("Introduza a idade:"))
+preco= [10, 20, 30]
 if idade<=10:
-    print(f"Crianca com {idade} anos")
+    print(f"Crianca com {idade} anos,\n paga {preco[0]} Meticais")
 elif idade>10 and idade<=17:
-    print(f"Adolescente com {idade} anos")
+    print(f"Adolescente com {idade} anos,\n paga {preco[1]} Meticais")
 else:
-    print(f"Adulto com {idade} anos")
+    print(f"Adulto com {idade} anos,\n paga {preco[2]} Meticais")
